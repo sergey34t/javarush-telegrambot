aa5927dde96aa46e50282fbcd2d536fb6278047f
Sergey Tsimanov <sergey.tsimanov@gmail.com>
2021-04-16 22:56:42 +0300

    JRTB-0: create Skeleton Spring boot project

---
+++ src/test/java/com/github/javaruncommunity/jrtb/JavarunTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javaruncommunity.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarunTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
