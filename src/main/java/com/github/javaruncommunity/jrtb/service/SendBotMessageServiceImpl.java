a9e96fc6910613057ca947caf88b7c90fd5d60e7
sergey34t <5eR9eyKT>
2021-04-20 11:38:23 +0300

    JRTB-3: added Command pattern for handling Telegram Bot commands

---
+++ src/main/java/com/github/javaruncommunity/jrtb/service/SendBotMessageServiceImpl.java
@@ -0,0 +1,32 @@
+package com.github.javaruncommunity.jrtb.service;
+
+import com.github.javaruncommunity.jrtb.bot.JavaRunTelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class SendBotMessageServiceImpl implements SendBotMessageService{
+
+    private final JavaRunTelegramBot javaRunTelegramBot;
+
+    @Autowired
+    public SendBotMessageServiceImpl(JavaRunTelegramBot javaRunTelegramBot) {
+        this.javaRunTelegramBot = javaRunTelegramBot;
+    }
+
+    @Override
+    public void sendMessage(String chatId, String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.enableHtml(true);
+        sendMessage.setText(message);
+
+        try {
+            javaRunTelegramBot.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            //todo add logging to the project.
+            e.printStackTrace();
+        }
+    }
+
+}
